fe498d706118549b0bb5f7bfc7903abb5e2a9682
Maxwell Fischer <19.maxwell.fischer@ksd.org>
2018-11-28 13:58:38 -0800

    Initialized Unity Project

---
+++ Assets/PlayController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5f;
+
+    private PlayerMotor motor;
+
+    private void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    private void Update()
+    {
+        //Calculate movement velocity as a 3D vector
+        float _xMov = Input.GetAxisRaw("Horizontal");
+        float _zMov = Input.GetAxisRaw("Vertical");
+
+        Vector3 _movHorizontal = transform.right * _xMov;
+        Vector3 _movVertical = transform.forward * _zMov;
+
+        //Final movement vector
+        Vector3 _velocity = (_movHorizontal + _movVertical).normalized * speed;
+        motor.Move(_velocity);
+    }
+
+}
---
+++ Assets/PlayerMotor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour
+{
+    private Vector3 velocity = Vector3.zero;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    //Gets a movement vector
+    public void Move (Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    //run every physics iteration
+    private void FixedUpdate()
+    {
+        PerformMovement();
+    }
+
+    //Perform movement based on velocity variable
+    void PerformMovement()
+    {
+        if(velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+    }
+
+}
